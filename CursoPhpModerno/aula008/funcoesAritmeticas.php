a0cd731c94e14cd9872028251b426632deefd677
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-09-09 11:44:54 -0300

    Ajustes das pastas

---
+++ CursoPhpModerno/aula008/funcoesAritmeticas.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções Matemáticas</title>
+</head>
+<body>
+    <!-- 
+abs(): Retorna o valor absoluto de um número.
+round(): Arredonda um número para o inteiro mais próximo.
+floor(): Arredonda um número para baixo, para o inteiro mais próximo.
+ceil(): Arredonda um número para cima, para o inteiro mais próximo.
+sqrt(): Retorna a raiz quadrada de um número.
+pow(): Calcula a potência de um número.
+max(): Retorna o maior valor entre os argumentos fornecidos.
+min(): Retorna o menor valor entre os argumentos fornecidos.
+pi(): Retorna o valor de π (pi).
+rand(): Gera um número inteiro aleatório.
+mt_rand(): Gera um número inteiro aleatório com melhor desempenho.
+    -->
+</body>
+</html>
