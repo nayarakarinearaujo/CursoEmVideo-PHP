4653ac254079e0cd22fc821ad7c683edfc80bf56
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-09-09 11:11:44 -0300

    Funções aritmeticas

---
+++ aula007/expressoesAritmeticas.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- 
+
+ORDEM DE PRECEDÊNCIA PHP
+
+( ) : Modificadores de ordem de precedência
+  
+ ** : Potência
+ * / % : MUltiplicação, Divisão, Módulos
+ + - : Adição, Subtração
+
+Reolver sempre da esquerda para direta na ordem que aparecer, 
+isso quando não tiver parênteses.
+
+// * PARA ALTERAR A ORDEM DE PRECEDÊNCIA COLOCAR PARÊNTESES.
+
+    -->
+</body>
+
+</html>
