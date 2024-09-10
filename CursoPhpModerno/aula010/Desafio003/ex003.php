779cc1cb6e3d91ccae2b60d9ef7d5a93c90c10ed
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-09-10 16:44:12 -0300

    Desafio003

---
+++ CursoPhpModerno/aula010/Desafio003/ex003.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversor de moedas</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<!-- 5,66 -->
+
+<section>
+        <h1>Trabalhando com números aleatórios</h1>
+        
+        <form action="ex002.php" method="get">
+
+            <?php
+            
+            ?>
+
+            <input type="submit" value="Gerar número aleatório">
+
+        </form>
+    </section>
+</body>
+</html>
