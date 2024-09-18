054c57bc6cd0133208e09dc7d42c3ed241f303a1
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-09-18 09:11:20 -0300

    Desafio 05

---
+++ CursoPhpModerno/aula010/Desafio005/numero.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Analisador de Números Reais</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+    <h1>Analisador de Número Real</h1>
+<?php
+
+
+
+?>
+    </main>
+</body>
+</html>
