1a5da44069a17127cd04f8c5a6b7102dc3400bd2
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-09-18 09:23:05 -0300

    Feito a lógica no arquivo php

--- CursoPhpModerno/aula010/Desafio005/numero.php
+++ CursoPhpModerno/aula010/Desafio005/numero.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,14 +8,26 @@
 
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <main>
-    <h1>Analisador de Número Real</h1>
-<?php
+        <h1>Analisador de Número Real</h1>
 
+        <?php
+        $num = $_POST["n"] ?? 0;
 
+        echo "<p>Analisando o número <strong>" . number_format($num, 3, ",", ".") . "</strong> informado pelo usuário: </p>";
 
-?>
+        $int = (int) $num;
+        $fra = $num - $int;
+
+        echo "<ul><li>A parte inteira do número é: <strong>". number_format($int, 0, ",", ".")."</strong></li>";
+
+        echo "<li>A parte fracionada do número é: <strong>". number_format($fra, 3, ",", ".")."</strong></li></ul>";
+
+        ?>
+        <button onclick="javascript:history.go(-1)">Voltar</button>
     </main>
 </body>
+
 </html>
