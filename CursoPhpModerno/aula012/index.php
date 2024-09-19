25376411e38ceba56a021930a9a29bdc0d0269fc
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-09-19 09:18:44 -0300

    Formulários

---
+++ CursoPhpModerno/aula012/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício PHP</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <form action="" method="get">
+            
+        </form>
+    </main>
+</body>
+</html>
